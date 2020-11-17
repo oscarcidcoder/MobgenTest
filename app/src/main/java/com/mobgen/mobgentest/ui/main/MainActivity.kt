7d3d0a8515e73e652634308e4efc6b7560543749
oscarcid <oscid88@gmail.com>
2020-11-17 19:28:01 +0100

    MobgenApplication class

--- app/src/main/java/com/mobgen/mobgentest/ui/main/MainActivity.kt
+++ app/src/main/java/com/mobgen/mobgentest/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mobgen.mobgentest
+package com.mobgen.mobgentest.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mobgen.mobgentest.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
