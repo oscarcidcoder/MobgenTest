4a87d099670d11ffeef217df5fe7c8abae32d9f2
oscarcid <oscid88@gmail.com>
2020-11-18 12:10:39 +0100

    Repository pattern to handle data

---
+++ app/src/main/java/com/mobgen/mobgentest/repository/RepositoryMobgen.kt
@@ -0,0 +1,7 @@
+package com.mobgen.mobgentest.repository
+
+import com.mobgen.mobgentest.db.dao.CategoryDAO
+import com.mobgen.mobgentest.network.NetworkDataSourse
+
+class RepositoryMobgen(remote: NetworkDataSourse, local: CategoryDAO) {
+}
